0f43312b7680dca1accccf1a765078ec6ff6c4cb
Ju Chen <cju@bombur.cs.ucr.edu>
2020-11-10 18:45:53 -0800

    more ops
    Signed-off-by: Ju Chen <cju@bombur.cs.ucr.edu>

--- src/op_def.rs
+++ src/op_def.rs
@@ -53,7 +53,38 @@ pub enum RGD {
   Memcmp,
 }
 
-
-pub enum DFSAN {
-  Bool = 0,
-}
+//Derived from llvm-6.0/llvm/IR/Instruction.def 
+//and dfsan.h
+pub const DFSAN_Read: u32 = 0;
+pub const DFSAN_Not: u32 = 1;
+pub const DFSAN_Neg: u32 = 2;
+pub const DFSAN_Add: u32 = 11;
+pub const DFSAN_Sub: u32 = 13;
+pub const DFSAN_Mul: u32 = 15;
+pub const DFSAN_UDiv: u32 = 17;
+pub const DFSAN_SDiv: u32 = 18;
+pub const DFSAN_URem: u32 = 20;
+pub const DFSAN_SRem: u32 = 21;
+pub const DFSAN_Shl: u32 = 23;
+pub const DFSAN_LShr: u32 = 24;
+pub const DFSAN_AShr: u32 = 25;
+pub const DFSAN_And: u32 = 26;
+pub const DFSAN_Or: u32 = 27;
+pub const DFSAN_Xor: u32 = 28;
+pub const DFSAN_Trunc: u32 = 36;
+pub const DFSAN_ZExt: u32 = 37;
+pub const DFSAN_SExt: u32 = 38;
+pub const DFSAN_Load: u32 = 67;
+pub const DFSAN_Extract: u32 = 68;
+pub const DFSAN_Concat: u32 = 69;
+//relational
+pub const DFSAN_bveq: u32 = 32;
+pub const DFSAN_bvneq: u32 = 33;
+pub const DFSAN_bvugt: u32 = 34;
+pub const DFSAN_bvuge: u32 = 35;
+pub const DFSAN_bvult: u32 = 36;
+pub const DFSAN_bvule: u32 = 37;
+pub const DFSAN_bvsgt: u32 = 38;
+pub const DFSAN_bvsge: u32 = 39;
+pub const DFSAN_bvslt: u32 = 40;
+pub const DFSAN_bvsle: u32 = 41;
