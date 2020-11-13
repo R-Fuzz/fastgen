1557a9ff6c5ee1d15a612c35e1972410815b81bc
Ju Chen <cju@bombur.cs.ucr.edu>
2020-11-12 21:18:20 -0800

    Pipe reading and parsing
    Signed-off-by: Ju Chen <cju@bombur.cs.ucr.edu>

--- src/fifo.rs
+++ src/fifo.rs
@@ -1,11 +1,33 @@
 use nix::unistd;
 use nix::sys::stat;
+//use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 
 pub fn make_pipe() {
   match unistd::mkfifo("/tmp/wp", stat::Mode::S_IRWXU) {
     Ok(_) => println!("created"),
     Err(err) => println!("Error creating fifo: {}", err),
   }
+}
+
+pub fn read_pipe() -> Vec<u32> {
+  let f = File::open("/tmp/wp").expect("open pipe failed");
+  let mut reader = BufReader::new(f);
+  let mut ret = Vec::new();
+  loop {
+    let mut buffer = String::new();
+    let num_bytes = reader.read_line(&mut buffer).expect("read pipe failed");
+    //if not EOF
+    if num_bytes !=0  {
+      let label = buffer.trim().parse::<u32>().expect("we expect u32 number in each line");
+      ret.push(label); 
+    } else  {
+      break;
+    }
+  }
+  ret
 }
 
 #[cfg(test)]
@@ -16,4 +38,11 @@ mod tests {
   fn test_make_pipe() {
     make_pipe()
   }
+
+  #[test]
+  fn test_read_pipe() {
+    let v = read_pipe();
+    println!("{:?}", v);
+  }
+
 }
