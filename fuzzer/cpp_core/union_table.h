e0164a239dfc716df2e12ee194e20ed75faad77f
Your Name <you@example.com>
2021-07-10 07:26:45 +0000

    forgot
    Signed-off-by: Your Name <you@example.com>

---
+++ fuzzer/cpp_core/union_table.h
@@ -0,0 +1,14 @@
+typedef uint32_t dfsan_label;
+
+struct dfsan_label_info {
+  dfsan_label l1;
+  dfsan_label l2;
+  uint64_t op1;
+  uint64_t op2;
+  uint16_t op;
+  uint16_t size;
+  uint8_t flags;
+  uint32_t tree_size;
+  uint32_t hash;
+  uint32_t depth;
+} __attribute__((aligned (8)));
