6c0d610514cc96594a1da367e273aa5ecc166069
Ju Chen <chenju2k6@gmail.com>
2021-12-24 08:18:21 +0000

    Fix front-end crash due to library search path
    Signed-off-by: Ju Chen <chenju2k6@gmail.com>

--- fuzzer/build.rs
+++ fuzzer/build.rs
@@ -11,7 +11,7 @@ fn main() {
 
 //  println!(r"cargo:rustc-link-search=fuzzer/cpp_core/build");
 //  println!(r"cargo:rustc-link-search=/usr/lib/llvm-9/lib");
-//  println!(r"cargo:rustc-link-search=/usr/local/lib");
-//  println!(r"cargo:rustc-link-search=/out/lib");
+  println!(r"cargo:rustc-link-search=/usr/local/lib");
+  println!(r"cargo:rustc-link-search=/out/lib");
 
 }
