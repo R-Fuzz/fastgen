316292b0d29bb516345cb8b02e85cd1856def777
Ju Chen <cju@bombur.cs.ucr.edu>
2022-06-23 12:32:18 -0700

    inew
    Signed-off-by: Ju Chen <cju@bombur.cs.ucr.edu>

--- usenix/run_symsan_time.sh
+++ usenix/run_symsan_time.sh
@@ -1,7 +1,6 @@
-dir=$1 
-pro=$2
-opt=$3
-find ${dir} -name "id*" | while read line; do
-do
-  { time  ( TAINT_OPTIONS="taint_file=$line" ./${pro} ${opt} $line &> /dev/null ; )   }  2>&1  | grep real
-done
+ dir=$1
+ pro=$2
+ opt=$3
+ find ${dir} -name "id*" | while read line; do
+    { time  ( TAINT_OPTIONS="taint_file=$line" ./${pro}.symsan ${opt} $line &> /dev/null ; )   }  2>&1  | grep real
+ done
