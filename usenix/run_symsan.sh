94d1399e74a64ae1aec2e4198c0f2f09c15202f5
Ju Chen <cju@bombur.cs.ucr.edu>
2022-06-24 01:19:24 -0700

    make
    Signed-off-by: Ju Chen <cju@bombur.cs.ucr.edu>

--- usenix/run_symsan.sh
+++ usenix/run_symsan.sh
@@ -1,3 +1,4 @@
 cat /src/cgc_list | while read line; do
-{ time TAINT_OPTIONS=taint_file=/out/cgc_seeds/$line/seed timeout -k 10 300 ./challenges/$line/$line < /out/cgc_seeds/$line/seed 1>/dev/null 2>/dev/null; } 2>&1 | grep real
+mkdir -p ${line}_outseeds
+{ time TAINT_OPTIONS="taint_file=/out/cgc_seeds/$line/seed,output_dir=${line}_outseeds" timeout -k 10 300 ./challenges/$line/$line < /out/cgc_seeds/$line/seed 1>/dev/null 2>/dev/null; } 2>&1 | grep real
 done
