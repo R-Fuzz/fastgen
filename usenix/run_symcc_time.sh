89837b04dd0a86623ccedf55632f0617d788cf54
Ju Chen <chenju2k6@gmail.com>
2022-07-08 23:39:03 +0000

    update symcc scripts
    Signed-off-by: Ju Chen <chenju2k6@gmail.com>

--- usenix/run_symcc_time.sh
+++ usenix/run_symcc_time.sh
@@ -1,7 +1,9 @@
 dir=$1 
 pro=$2
 opt=$3
-mkdir -p ${pro}_outseeds
+count=0
 find ${dir} -name "id*" | while read line; do
-  { time SYMCC_INPUT_FILE=$line SYMCC_ENABLE_LINEARIZATION=1 SYMCC_OUTPUT_DIR=${pro}_outseeds ./${pro}.symcc ${opt} ${line} 1>/dev/null 2>/dev/null; } 2>&1 | grep real
+  mkdir -p ${pro}_outseeds_${count}
+  { time SYMCC_INPUT_FILE=$line SYMCC_ENABLE_LINEARIZATION=1 SYMCC_OUTPUT_DIR=${pro}_outseeds_${count} ./${pro}.symcc ${opt} ${line} 1>/dev/null 2>/dev/null; } 2>&1 | grep real
+  count=$((count+1))
 done
